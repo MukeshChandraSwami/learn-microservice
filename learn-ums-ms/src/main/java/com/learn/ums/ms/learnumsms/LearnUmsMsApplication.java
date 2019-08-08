4adc299db15e5b8de8d40fd212cd651396e0269d
Mukesh CHANDRA <G522141@mph.morpho.com>
2019-08-08 23:46:45 +0530

    Added UMS

---
+++ learn-ums-ms/src/main/java/com/learn/ums/ms/learnumsms/LearnUmsMsApplication.java
@@ -0,0 +1,13 @@
+package com.learn.ums.ms.learnumsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnUmsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnUmsMsApplication.class, args);
+	}
+
+}
