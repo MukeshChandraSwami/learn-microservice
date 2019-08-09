3fdefd8ec2d286d8b0b645bc36af5379e66bd43a
Mukesh CHANDRA <G522141@mph.morpho.com>
2019-08-09 09:20:25 +0530

    Added API gateway

---
+++ book-api-gateway/src/main/java/com/learn/api/gateway/bookapigateway/BookApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.learn.api.gateway.bookapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookApiGatewayApplication.class, args);
+	}
+
+}
