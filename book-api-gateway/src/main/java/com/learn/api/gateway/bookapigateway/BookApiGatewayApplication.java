17e2a8ddc0263537d05e8bedbeb14bba2ec25262
Mukesh CHANDRA <G522141@mph.morpho.com>
2019-08-09 09:40:52 +0530

    Registered as zuul api gateway and client micro service

--- book-api-gateway/src/main/java/com/learn/api/gateway/bookapigateway/BookApiGatewayApplication.java
+++ book-api-gateway/src/main/java/com/learn/api/gateway/bookapigateway/BookApiGatewayApplication.java
@@ -2,8 +2,12 @@ package com.learn.api.gateway.bookapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class BookApiGatewayApplication {
 
 	public static void main(String[] args) {
